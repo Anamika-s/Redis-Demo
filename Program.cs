352f440bde8deb44779bc7c491f2c10304bd2034
anamikasawhney <anamika.sawhney22@gmail.com>
2021-09-21 11:06:02 +0530

    Redis Demo

---
+++ Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using StackExchange.Redis;
+using System.Threading.Tasks;
+
+
+
+namespace RedisDemo
+{
+    class Program
+    {
+        static async Task Main(string[] args)
+        {
+            string connectionString="redisdemo2109.redis.cache.windows.net:6380,password=OG89gr2BrvR5pMgvBALOt2r4MHQwkbpL4fn1K+3YAlA=,ssl=True,abortConnect=False";
+            using (var cache = ConnectionMultiplexer.Connect(connectionString))
+            {
+                IDatabase db = cache.GetDatabase();
+
+                bool setValue = await db.StringSetAsync("test:demokey", "100");
+                Console.WriteLine($"SET: {setValue}");
+
+                string getValue = await db.StringGetAsync("test:demokey");
+                Console.WriteLine($"GET: {getValue}");
+                
+                bool deleted= await db.KeyDeleteAsync("test:demokey");
+                Console.WriteLine($"DELETED: {deleted}");
+                
+            }
+
+        }
+    }
+}
